ca9a59eb5c3ca26f8aec5ff70ca76c4d9f134dc2
Celdari <ari.suoyrjo@gmail.com>
2014-09-17 15:39:00 +0300

    Netbeans project creation

---
+++ Etaprojekti2014/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: user
+ *
+ * Created on September 17, 2014, 3:24 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
