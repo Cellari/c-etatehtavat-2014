32959ed570976e0432b1f380c38d3409e0a7e7ba
Ari Suoyrjö <ari.suoyrjo@gmail.com>
2014-09-19 16:43:47 +0300

    t7 done

---
+++ t7/t7.c
@@ -0,0 +1,39 @@
+/* 
+ * File:   t7.c
+ * Author: Ari Suoyrjö
+ *
+ * Created on September 19, 2014, 2:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    char merkki;
+    char jono[] = "taateli";
+    char *p;
+    int c;
+    
+    printf("Päivän sana on 'taateli'. Anna yksi merkki\n");
+    while (1) {        
+        scanf(" %c", &merkki); //Tyhjä välilyönti syö \n merkin
+
+//        printf("Annoit: %c\n", merkki);
+
+        p = strchr(jono, merkki);
+        
+        if(p != NULL) printf("p = %i\n", p);
+        else {
+            printf("Merkkiä ei löytynyt. Ohjelma lopetetaan\n");
+            break;
+        }
+        
+    }
+    
+    return (EXIT_SUCCESS);
+}
+
