deb4e1a48d31399187f0b6223975b91999b2a12f
Ari Suoyrjö <ari.suoyrjo@gmail.com>
2014-09-19 16:45:45 +0300

    t8 start

---
+++ t8/t8.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   t8.c
+ * Author: user
+ *
+ * Created on September 19, 2014, 4:45 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
