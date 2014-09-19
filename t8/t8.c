30beebc369653752e58ebf8978a16efa13b2ac70
Ari Suoyrjö <ari.suoyrjo@gmail.com>
2014-09-19 16:49:01 +0300

    t8 done

--- t8/t8.c
+++ t8/t8.c
@@ -1,18 +1,42 @@
 /* 
  * File:   t8.c
- * Author: user
+ * Author: Ari Suoyrjö
  *
- * Created on September 19, 2014, 4:45 PM
+ * Created on September 19, 2014, 2:42 PM
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+    char merkki;
+    char jono[10];
+    char *p;
+    int c;
+    
+    printf("Anna sana: ");
+    fgets(jono, sizeof(jono), stdin);
+    
+    printf("Päivän sana on '%s'. Anna yksi merkki\n", jono);
+    while (1) {        
+        scanf(" %c", &merkki); //Tyhjä välilyönti syö \n merkin
 
+//        printf("Annoit: %c\n", merkki);
+
+        p = strchr(jono, merkki);
+        
+        if(p != NULL) printf("p = %i\n", p);
+        else {
+            printf("Merkkiä ei löytynyt. Ohjelma lopetetaan\n");
+            break;
+        }
+        
+    }
+    
     return (EXIT_SUCCESS);
 }
 
