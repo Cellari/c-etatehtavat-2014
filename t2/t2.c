bfd60100263425fc5ba9cfc0e9506981c8530995
Ari Suoyrjö <ari.suoyrjo@gmail.com>
2014-09-19 11:27:08 +0300

    Exercise 2 start

---
+++ t2/t2.c
@@ -0,0 +1,27 @@
+/* 
+ * File:   t2.c
+ * Author: user
+ *
+ * Created on September 18, 2014, 7:05 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int i;
+    
+    if(argc != 2) {
+        printf("Usage: t2 [source] [destination]\n");
+        printf("\nCopies source file to destination\n");
+    }
+    else {
+        
+    }
+    
+    return (EXIT_SUCCESS);
+}
+
