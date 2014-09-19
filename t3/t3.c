a3ab74c7812990c767772d1b517ddf35ae3ac1c9
Ari Suoyrjö <ari.suoyrjo@gmail.com>
2014-09-19 12:49:50 +0300

    t3 done

--- t3/t3.c
+++ t3/t3.c
@@ -12,7 +12,27 @@
  * 
  */
 int main(int argc, char** argv) {
-
+    char ch;
+    FILE *source;
+    int count = 0;
+    
+    if(argc != 2) {
+        printf("Usage: t3 [source]\n");
+        printf("\nCounts every character in the given source file\n");
+    }
+    else {
+        source = fopen(argv[1], "r");
+        if(source == NULL) {
+            printf("No file found. Terminating session\n");
+            exit(EXIT_FAILURE);
+        }
+        
+        while( ( ch = fgetc(source) ) != EOF )
+            count++;
+        
+        printf("File had %i characters.\n", count);
+ 
+        fclose(source);
+    }
     return (EXIT_SUCCESS);
 }
-
